a0d0a55a734e4097f3091b5909f8d665682375a1
z.lewis <z.lewis.digipen@digipen.edu>
2017-11-30 16:26:04 -0800

    Added some functions to file reader

--- Procedural_Generation/Assets/Scripts/FileReader.cs
+++ Procedural_Generation/Assets/Scripts/FileReader.cs
@@ -23,27 +23,36 @@ public class Table
         for (int i = 0; i < _content.Count; i++)
             content.Add(_content[i]);
     }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public List<string> GetContent()
+    {
+        return content;
+    }
 }
 
 public class FileReader : MonoBehaviour {
 
-    private List<Table> tables;
+    public List<Table> tables;
 
-	// Use this for initialization
-	void Start () {
+    void Awake()
+    {
         string path = Application.dataPath + "/Tables/";
 
         string[] all_files = Directory.GetFiles(path, "*.txt");
         tables = new List<Table>();
 
-        foreach(string file in all_files)
+        foreach (string file in all_files)
         {
             List<string> content = new List<string>();
             StreamReader reader = new StreamReader(file);
             string name = Path.GetFileName(file);
             string line;
-
-            Debug.Log(name);
+            
 
             do
             {
@@ -53,14 +62,43 @@ public class FileReader : MonoBehaviour {
                     content.Add(line);
                 }
             } while (line != null);
-            
+
             tables.Add(new Table(name, content));
         }
-
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    //Checks if the File exist
+    public bool FindFile(string name)
+    {
+        for(int i = 0; i < tables.Count; i++)
+        {
+            if (name == tables[i].GetName())
+                return true;
+        }
+
+        return false;
+    }
+
+    //Returns a line from the rolldice thing
+    public string RollDice(string name)
+    {
+        int ind = -1;
+
+        //Loop to get index of the file
+        for (int i = 0; i < tables.Count; i++)
+        {
+            if (name == tables[i].GetName())
+            {
+                ind = i;
+                break;
+            }
+        }
+
+        if (ind == -1)
+            return "";
+
+        int rand_num = Random.Range(0, tables[ind].GetContent().Count - 1);
+
+        return tables[ind].GetContent()[rand_num];
+    }
 }
--- Procedural_Generation/Assets/Scripts/TileGeneration.cs
+++ Procedural_Generation/Assets/Scripts/TileGeneration.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class TileGeneration : MonoBehaviour {
 
+    private FileReader fm;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        fm = this.GetComponent<FileReader>();
+
+        //Debug.Log(fm.RollDice("Doors.txt"));
+
+        /*
+         * Make a tile, then do a loop, call fm.FindFile(name), name = name of file, at the start it should be Doors.txt
+         *  in loop call fm.RollDice(name)
+         *  you will get return of string, which is one of the things in the file
+         *  from then we should talk what it should do, I am thinking just a long if statement thing for different words
+         *  
+         *  you will have to create another loop that says something like, for how many doors create a tile and stuff like that
+         */
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        
+    }
 }
